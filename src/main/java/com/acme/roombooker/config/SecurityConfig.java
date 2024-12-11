13f951abffc9bff16c72f08b3ec8112aef0307d9
johnpapwinter <johnpapahimonas@yahoo.com>
2024-12-11 11:42:59 +0200

    added actuator

--- src/main/java/com/acme/roombooker/config/SecurityConfig.java
+++ src/main/java/com/acme/roombooker/config/SecurityConfig.java
@@ -51,6 +51,10 @@ public class SecurityConfig {
                 .addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/actuator/**"
+                        )
+                        .permitAll()
+                        .requestMatchers(
                                 SWAGGER_WHITELIST
                         )
                         .permitAll()
